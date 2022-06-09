9bc33aa75f47d2a619c6674f7f20cd3049c0b4f0
Sirbu Nicolae-Cezar <sirbunicolaecezar@gmail.com>
2022-06-09 12:47:48 +0300

    Add projeect

---
+++ dist/esm/index.js
@@ -0,0 +1,8 @@
+import { jsx as _jsx, Fragment as _Fragment, jsxs as _jsxs } from "react/jsx-runtime";
+import React from 'react';
+import { Button } from '@mantine/core';
+export class HappiGraph extends React.Component {
+    render() {
+        return (_jsxs(_Fragment, { children: [_jsx("div", { children: "Hello from components-library" }), _jsx(Button, { children: "Hello" })] }));
+    }
+}
---
+++ dist/types/index.d.ts
@@ -0,0 +1,9 @@
+import React from 'react';
+interface Props {
+}
+interface State {
+}
+export declare class HappiGraph extends React.Component<Props, State> {
+    render(): JSX.Element;
+}
+export {};
